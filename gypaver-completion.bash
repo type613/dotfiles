6eb310dfc5623b02fc96bdbc0b48f8e0b7bd1389
Yoshinori Kitagawa <yoshinori.kitagawa@optim.co.jp>
2015-01-15 15:48:45 +0900

    bug fix  * paver -> gypaver changed.

--- gypaver-completion.bash
+++ gypaver-completion.bash
@@ -1,14 +1,14 @@
 # see https://groups.google.com/forum/#!topic/paver/Ba5YNXNhs9U
 
-_paver()
+_gypaver()
 {
     local cur
     COMPREPLY=()
     # Variable to hold the current word
     cur="${COMP_WORDS[COMP_CWORD]}"
 
-    # Build a list of the available tasks from: `paver --help --quiet`
-    local cmds=$(paver -hq | awk '/^  ([a-zA-Z][a-zA-Z0-9_]+)/ {print $1}')
+    # Build a list of the available tasks from: `gypaver --help --quiet`
+    local cmds=$(gypaver -hq | awk '/^  ([a-zA-Z][a-zA-Z0-9_]+)/ {print $1}')
 
     # Generate possible matches and store them in the
     # array variable COMPREPLY
@@ -16,4 +16,4 @@ _paver()
 }
 
 # Assign the auto-completion function for our command.
-complete -F _paver paver
+complete -F _gypaver gypaver
